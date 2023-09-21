6432e0ae46ae1b74990d8f233d145d7d0edf4aa0
dengpju <dengpju@163.com>
2023-09-21 16:18:15 +0800

    更新

--- him/paginate.go
+++ him/paginate.go
@@ -1,9 +1,9 @@
 package him
 
 type Paginate struct {
-	Total       uint64
-	PerPage     uint64
-	CurrentPage uint64
-	LastPage    uint64
-	Items       interface{}
+	Total       uint64      `json:"total"`
+	PerPage     uint64      `json:"per_page"`
+	CurrentPage uint64      `json:"current_page"`
+	LastPage    uint64      `json:"last_page"`
+	Items       interface{} `json:"items"`
 }
