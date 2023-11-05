230645c2033cd3a37016dd5588ecdd7d2481fe79
dengpengju <dengpju@163.com>
2023-11-05 22:43:18 +0800

    更新

--- arm/IModel.go
+++ arm/IModel.go
@@ -2,6 +2,7 @@ package arm
 
 import (
 	"database/sql"
+	"encoding/json"
 	"github.com/dunpju/higo-orm/him"
 	"gorm.io/gorm"
 )
@@ -15,4 +16,14 @@ type IModel interface {
 	NewModel(properties ...him.IProperty) IModel
 	Begin(opts ...*sql.TxOptions) *him.TX
 	TX(tx *gorm.DB) *Model
+}
+
+type Models string
+
+func MakeModels(v interface{}) Models {
+	b, err := json.Marshal(v)
+	if err != nil {
+		panic(err)
+	}
+	return Models(b)
 }
