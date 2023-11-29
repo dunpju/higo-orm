c049d31c8ad78feee6cd04d58f0fb7c8e3dbaac9
dengpengju <dengpju@163.com>
2023-11-29 11:22:44 +0800

    更新

--- arm/Fields.go
+++ arm/Fields.go
@@ -1,33 +1,44 @@
 package arm
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+)
+
+var (
+	backQuoteReg = regexp.MustCompile("`")
+)
 
 type Fields string
 
 func (this Fields) Pre(pre string) Fields {
-	return Fields(fmt.Sprintf("%s.%s", pre, this))
+	return Fields(fmt.Sprintf("`%s`.`%s`", pre, this))
 }
 
 func (this Fields) AS(as string) string {
-	return fmt.Sprintf("%s AS %s", this, as)
+	return fmt.Sprintf("`%s` AS %s", this, as)
 }
 
 func (this Fields) ASC() string {
-	return fmt.Sprintf("%s ASC", this)
+	return fmt.Sprintf("`%s` ASC", this)
 }
 
 func (this Fields) DESC() string {
-	return fmt.Sprintf("%s DESC", this)
+	return fmt.Sprintf("`%s` DESC", this)
 }
 
 func (this Fields) COUNT() Fields {
-	return Fields(fmt.Sprintf("COUNT(%s)", this))
+	return Fields(fmt.Sprintf("COUNT(`%s`)", this))
 }
 
 func (this Fields) SUM() Fields {
-	return Fields(fmt.Sprintf("SUM(%s)", this))
+	return Fields(fmt.Sprintf("SUM(`%s`)", this))
 }
 
 func (this Fields) String() string {
-	return string(this)
+	isMatch := backQuoteReg.Match([]byte(this))
+	if isMatch {
+		return string(this)
+	}
+	return fmt.Sprintf("`%s`", string(this))
 }
