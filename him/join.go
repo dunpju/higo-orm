c54af68d950799399e5138c85f55ab82e654e3e9
dengpengju <dengpju@163.com>
2023-12-01 14:01:11 +0800

    更新

--- him/join.go
+++ him/join.go
@@ -3,6 +3,7 @@ package him
 import (
 	"fmt"
 	"github.com/Masterminds/squirrel"
+	"strings"
 )
 
 type joinCase int
@@ -20,23 +21,39 @@ type join struct {
 	rest  []interface{}
 }
 
-func (this *SelectBuilder) Join(table, first any, operator string, second any, rest ...any) *SelectBuilder {
-	this.joins = append(this.joins, join{Join, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), rest})
+func (this *SelectBuilder) Join(table, first any, operator string, second any, options ...any) *SelectBuilder {
+	if len(options) > 0 {
+		this.joins = append(this.joins, join{Join, fmt.Sprintf("%s ON %s %s %s %s", table, columnToString(first), operator, columnToString(second), strings.Join(columnsToString(options...), " ")), nil})
+	} else {
+		this.joins = append(this.joins, join{Join, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), nil})
+	}
 	return this
 }
 
-func (this *SelectBuilder) LeftJoin(table, first any, operator string, second any, rest ...any) *SelectBuilder {
-	this.joins = append(this.joins, join{LeftJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), rest})
+func (this *SelectBuilder) LeftJoin(table, first any, operator string, second any, options ...any) *SelectBuilder {
+	if len(options) > 0 {
+		this.joins = append(this.joins, join{LeftJoin, fmt.Sprintf("%s ON %s %s %s %s", table, columnToString(first), operator, columnToString(second), strings.Join(columnsToString(options...), " ")), nil})
+	} else {
+		this.joins = append(this.joins, join{LeftJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), nil})
+	}
 	return this
 }
 
-func (this *SelectBuilder) RightJoin(table, first any, operator string, second any, rest ...any) *SelectBuilder {
-	this.joins = append(this.joins, join{RightJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), rest})
+func (this *SelectBuilder) RightJoin(table, first any, operator string, second any, options ...any) *SelectBuilder {
+	if len(options) > 0 {
+		this.joins = append(this.joins, join{RightJoin, fmt.Sprintf("%s ON %s %s %s %s", table, columnToString(first), operator, columnToString(second), strings.Join(columnsToString(options...), " ")), nil})
+	} else {
+		this.joins = append(this.joins, join{RightJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), nil})
+	}
 	return this
 }
 
-func (this *SelectBuilder) InnerJoin(table, first any, operator string, second any, rest ...any) *SelectBuilder {
-	this.joins = append(this.joins, join{InnerJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), rest})
+func (this *SelectBuilder) InnerJoin(table, first any, operator string, second any, options ...any) *SelectBuilder {
+	if len(options) > 0 {
+		this.joins = append(this.joins, join{InnerJoin, fmt.Sprintf("%s ON %s %s %s %s", table, columnToString(first), operator, columnToString(second), strings.Join(columnsToString(options...), " ")), nil})
+	} else {
+		this.joins = append(this.joins, join{InnerJoin, fmt.Sprintf("%s ON %s %s %s", table, columnToString(first), operator, columnToString(second)), nil})
+	}
 	return this
 }
 
